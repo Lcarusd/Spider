72647a6e772f68d1349df29e3cab8ea25798865b
Lcarused <donghao19950925@gmail.com>
2018-12-28 12:48:35 +0800

    纽约热门景点分析与可视化

--- hot_attractions_analysis_visualization.py
+++ hot_attractions_analysis_visualization.py
@@ -5,19 +5,20 @@ from bs4 import BeautifulSoup
 
 # 分析纽约热门景点并可视化
 
-url = 'https://cn.tripadvisor.com/Attractions-g60763-Activities-New_York_City_New_York.html'
+page_step = 15
 
-wb_data = requests.get(url)
-soup = BeautifulSoup(wb_data.text, "lxml")
+url = 'https://www.tripadvisor.cn/Attractions-g60763-Activities-oa{0}-New_York_City_New_York.html#FILTERED_LIST'.format(15)
+
+r = requests.get(url)
+soup = BeautifulSoup(r.content, "lxml")
 
 titles = soup.select("div.listing_title > a")
 imgs = soup.select("img[width='180']")
 cates = soup.select("div.p13n_reasoning_v2")
 
 for title, img, cate in zip(titles, imgs, cates):
+    print ("test")
     data = {
         "景点名称": title.get_text(),
         "标签 ": list(cate.stripped_strings),
     }
-    # print data["景点名称"]
-    print data["标签 "][0]
