74c24c381a237a5775100dac3b008aec5963389e
Lcarused <donghao19950925@gmail.com>
2019-02-26 18:39:15 +0800

    请求参数工具

---
+++ tools/request_tools.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+
+import random
+
+class RequestTool(object):
+
+    def get_random_ua(self):
+        user_agents = ["Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1623.0 Safari/537.36",
+            "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; Media Center PC 6.0; InfoPath.3; MS-RTC LM 8; Zune 4.7)",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:29.0) Gecko/20120101 Firefox/29.0",
+            "Opera/9.80 (X11; Linux x86_64; U; Ubuntu/10.10 (maverick); pl) Presto/2.7.62 Version/11.01",
+            "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_6; it-it) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27"
+        ]
+
+        return random.choice(user_agents)
+
+request_tool = RequestTool()
