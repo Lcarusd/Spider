217cac69f05656bc8f77a629366c46153e732016
Lcarusd <1571239133@qq.com>
2019-05-13 17:13:38 +0800

    火柴盒用户信息抓取

--- huochaihe/user_info_spider.py
+++ huochaihe/user_info_spider.py
@@ -4,21 +4,18 @@ import json
 import random
 import time
 import os
+import logging
+import requests
 
-from huochaihe.tools import get_register_id, get_udid, requests_post
-from settings import con_redis_0
+from huochaihe.tools import get_register_id, get_udid, requests_post, get_headers, get_proxies, logger
+from settings import con_redis_0, con_redis_1
 
 class UserInfoSpider(object):
     def __init__(self):
         self.url  = "http://soa.matchbox.72zhe.com/v1/user/info"
-        self.data = {}
 
     def get_uid_task(self):
-        if con_redis_0.llen("huochaihe_tasks_1") != 0:
-            value_task = con_redis_0.lpop("huochaihe_tasks_1")
-            return value_task.decode("utf-8")
-        else:
-            return None
+        return con_redis_0.get("user_id_count").decode("utf-8")
 
     def get_random_account(self):
         length = con_redis_0.llen("huochaihe_account")
@@ -32,7 +29,7 @@ class UserInfoSpider(object):
         if not value_task:
             return False
 
-        self.data = {
+        data = {
             "source": "APP",
             "uid": account["user_id"],
             "register_id": get_register_id(),
@@ -42,44 +39,42 @@ class UserInfoSpider(object):
             "version": "4.10.5",
             "token_key": account["token_key"]
         }
-        resp = requests_post(self.url, self.data)
-        return resp
+
+        resp = requests.post(self.url, data=json.dumps(data), headers=get_headers(), proxies=get_proxies())
+        return resp, value_task
 
     def start_process_data(self):
-        resp = self.mix_data()
-        content = json.loads(resp.content)
+        resp, value_task = self.mix_data()
+        try:
+            content = json.loads(resp.content)
+            print("用户信息抓取结果：\n%s" % content)
+        except:
+            return
+
+        if content["error_code"] == "20000":
+            con_redis_0.set("user_id_count", int(con_redis_0.get("user_id_count").decode("utf-8")) + 1)
+            print("账号%s不存在..." % value_task)
+
+        if not content["error_code"] == "0" and not content["error_code"] == "20000":
+            logger(content)
 
         if content["error_code"] == "0":
-            print(content)
-            try:
-                con_redis_0.rpush("huochaihe_user_info", str(content["data"]))
-            except:
-                raise
-            con_redis_0.rpush("huochaihe_user_id", content["data"]["user_id"])
+            if not con_redis_0.exists(content["data"]["user_id"]):
+                try:
+                    con_redis_0.rpush("huochaihe_user_info", str(content["data"]))
+                    con_redis_0.set("user_id_count", int(con_redis_0.get("user_id_count")) + 1)
+                    con_redis_1.set(content["data"]["user_id"], 0)
+                    print("用户信息抓取成功!")
+                except:
+                    raise
 
-
-        if content["error_code"] != "0":
-            content.update({
-                "requests_account":self.data["uid"],
-                "task_user_id":self.data["user_id"]})
-            log_msg = "[{0}]:{1}".format(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()), content)
-            print(content)
-            with open(os.getcwd() + "/huochaihe/huochaihe.log", mode="a") as f:
-                f.write(log_msg + "\n")
-
-    def clear_param(self):
-        self.data = {}
+        print("*" * 100)
 
 def main():
-    for _ in range(con_redis_0.llen("huochaihe_tasks_1") - 1):
-        try:
-            uis = UserInfoSpider()
-            uis.start_process_data()
-            uis.clear_param()
-        except Exception as e:
-            #TODO：输出到控制台和日志
+    uis = UserInfoSpider()
+    for _ in range(100000):
+        uis.start_process_data()
 
-            main()
 
 if __name__ == "__main__":
     main()
